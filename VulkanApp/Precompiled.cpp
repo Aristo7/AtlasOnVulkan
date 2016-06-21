80fc3e98f0f0f7451f8d3d273329ea1db1dedbc7
Olex Lozitskiy <mr.olexander@gmail.com>
2016-06-20 20:27:28 -0400

    Empty console project added.

---
+++ VulkanApp/Precompiled.cpp
@@ -0,0 +1,8 @@
+// Precompiled.cpp : source file that includes just the standard includes
+// VulkanApp.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "Precompiled.h"
+
+// TODO: reference any additional headers you need in Precompiled.H
+// and not in this file
---
+++ VulkanApp/VulkanApp.cpp
@@ -0,0 +1,9 @@
+// VulkanApp.cpp : Defines the entry point for the console application.
+//
+
+#include "Precompiled.h"
+
+int main()
+{
+    return 0;
+}
